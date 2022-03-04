818c2d89ca7fe87b12a6996c6024c63cff9f4f89
DEN SEMENOV <da_semenov@mail.ru>
2022-03-04 12:48:28 +0300

    inc7 after review

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/caarlos0/env/v6"
 	"github.com/spf13/pflag"
-	"os"
 )
 
 type AppConfig struct {
@@ -14,30 +13,13 @@ type AppConfig struct {
 }
 
 func (config *AppConfig) Init() error {
-	fmt.Println(os.Args)
-	if err := env.Parse(config); err != nil {
-		fmt.Println("unable to load server settings", err)
-		return err
-	}
-
 	pflag.StringVarP(&config.ServerAddress, "a", "a", config.ServerAddress, "Http-server address")
 	pflag.StringVarP(&config.BaseURL, "b", "b", config.BaseURL, "Base URL")
 	pflag.StringVarP(&config.FileStorage, "f", "f", config.FileStorage, "File storage path")
 	pflag.Parse()
-
-	if config.BaseURL == "" || config.FileStorage == "" || config.ServerAddress == "" {
-		if err := env.Parse(&config); err != nil {
-			fmt.Println("unable to load server settings", err)
-			return err
-		}
-	}
-
-	if config.BaseURL == "" {
-		config.BaseURL = "http://localhost:8080/"
-	}
-
-	if config.BaseURL[len(config.BaseURL)-1:] != "/" {
-		config.BaseURL += "/"
+	if err := env.Parse(config); err != nil {
+		fmt.Println("unable to load server settings", err)
+		return err
 	}
 	return nil
 }
