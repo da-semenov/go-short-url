11475d61abcf8fa024dc83d867cd77e4770ccb25
DEN SEMENOV <da_semenov@mail.ru>
2022-03-20 20:08:03 +0300

    inc11

--- internal/app/handlers/user_handler.go
+++ internal/app/handlers/user_handler.go
@@ -19,12 +19,14 @@ type UserService interface {
 }
 
 type UserHandler struct {
-	service       UserService
+	userService   UserService
+	service       Service
 	cryptoService CryptoService
 }
 
-func NewUserHandler(service UserService, cs CryptoService) *UserHandler {
+func NewUserHandler(userService UserService, service Service, cs CryptoService) *UserHandler {
 	var h UserHandler
+	h.userService = userService
 	h.service = service
 	h.cryptoService = cs
 	return &h
@@ -65,7 +67,7 @@ func (z *UserHandler) GetUserURLsHandler(w http.ResponseWriter, r *http.Request)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	res, err := z.service.GetURLsByUser(userID)
+	res, err := z.userService.GetURLsByUser(userID)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -88,9 +90,90 @@ func (z *UserHandler) GetUserURLsHandler(w http.ResponseWriter, r *http.Request)
 }
 
 func (z *UserHandler) PingHandler(w http.ResponseWriter, r *http.Request) {
-	if !z.service.Ping() {
+	if !z.userService.Ping() {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
+}
+
+func (z *UserHandler) DefaultHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Unsupported request type",
+		http.StatusMethodNotAllowed)
+}
+
+func (z *UserHandler) PostMethodHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := getRequestBody(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	userID, err := z.getTokenCookie(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(b) == 0 {
+		http.Error(w, "body can't be empty", http.StatusBadRequest)
+		return
+	}
+	res, _ := z.service.GetID(string(b))
+	err = z.userService.Save(userID, string(b), res)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	_, err = w.Write([]byte(res))
+	if err != nil {
+		panic("Can't write response")
+	}
+	return
+}
+
+func (z *UserHandler) PostShortenHandler(w http.ResponseWriter, r *http.Request) {
+	b, err := getRequestBody(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	userID, err := z.getTokenCookie(w, r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if len(b) == 0 {
+		http.Error(w, "Body can't be empty", http.StatusBadRequest)
+		return
+	} else {
+		var req urls.ShortenRequest
+		if err := json.Unmarshal(b, &req); err != nil {
+			http.Error(w, "json error", http.StatusBadRequest)
+			return
+		}
+		res, err := z.service.GetID(req.URL)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
+		err = z.userService.Save(userID, req.URL, res)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		result := urls.ShortenResponse{Result: res}
+		responseBody, err := json.Marshal(result)
+		if err != nil {
+			http.Error(w, "Can't serialize response", http.StatusBadRequest)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		_, err = w.Write(responseBody)
+		if err != nil {
+			http.Error(w, "Can't write response", http.StatusBadRequest)
+			return
+		}
+		return
+	}
 }
